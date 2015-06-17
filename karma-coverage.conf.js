460570680a71082c87eb8e461ed478be19455d2c
Maira Bello <maira.araujo@liferay.com>
2015-06-17 14:10:29 -0300

    Splits karma coverage config into a separate file

--- karma-coverage.conf.js
+++ karma-coverage.conf.js
@@ -1,3 +1,4 @@
+var isparta = require('isparta');
 var metal = require('gulp-metal');
 
 var babelOptions = {
@@ -18,7 +19,8 @@ module.exports = function (config) {
 		],
 
 		preprocessors: {
-			'src/**/*.js': ['babel', 'commonjs'],
+			'src/**/!(*.soy).js': ['coverage', 'commonjs'],
+			'src/**/*.soy.js': ['babel', 'commonjs'],
 			'bower_components/metal/**/*.js': ['babel', 'commonjs'],
 			'bower_components/metal-position/**/*.js': ['babel', 'commonjs'],
 			'test/**/*.js': ['babel', 'commonjs']
@@ -26,6 +28,19 @@ module.exports = function (config) {
 
 		browsers: ['Chrome'],
 
-		babelPreprocessor: {options: babelOptions}
+		reporters: ['coverage', 'progress'],
+
+		babelPreprocessor: {options: babelOptions},
+
+		coverageReporter: {
+			instrumenters: {isparta : isparta},
+			instrumenter: {'**/*.js': 'isparta'},
+			instrumenterOptions: {isparta: {babel: babelOptions}},
+			reporters: [
+				{type: 'html'},
+				{type: 'lcov', subdir: 'lcov'},
+				{type: 'text-summary'}
+			]
+		}
 	});
 }
