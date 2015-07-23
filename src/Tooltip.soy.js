7f781fcf15171b5b14a0bc0e9c121a925a625d44
Maira Bello <maira.araujo@liferay.com>
2015-07-23 15:51:44 -0300

    Updates compiled soy file

--- src/Tooltip.soy.js
+++ src/Tooltip.soy.js
@@ -19,7 +19,7 @@ if (typeof Templates.Tooltip == 'undefined') { Templates.Tooltip = {}; }
  * @suppress {checkTypes}
  */
 Templates.Tooltip.content = function(opt_data, opt_ignored, opt_ijData) {
-  return soydata.VERY_UNSAFE.ordainSanitizedHtml('<div id="' + soy.$$escapeHtmlAttribute(opt_data.id) + '" class="tooltip component ' + soy.$$escapeHtmlAttribute(opt_data.elementClasses) + '"><div class="tooltip-arrow"></div>' + Templates.Tooltip.inner(opt_data, null, opt_ijData) + '</div>');
+  return soydata.VERY_UNSAFE.ordainSanitizedHtml('<div id="' + soy.$$escapeHtmlAttribute(opt_data.id) + '" class="tooltip component' + soy.$$escapeHtmlAttribute(opt_data.elementClasses ? ' ' + opt_data.elementClasses : '') + '"><div class="tooltip-arrow"></div>' + Templates.Tooltip.inner(opt_data, null, opt_ijData) + '</div>');
 };
 if (goog.DEBUG) {
   Templates.Tooltip.content.soyTemplateName = 'Templates.Tooltip.content';
@@ -40,7 +40,7 @@ if (goog.DEBUG) {
   Templates.Tooltip.inner.soyTemplateName = 'Templates.Tooltip.inner';
 }
 
-Templates.Tooltip.content.params = ["elementClasses","id"];
+Templates.Tooltip.content.params = ["id"];
 Templates.Tooltip.inner.params = ["content","id"];
 export default Templates.Tooltip;
 /* jshint ignore:end */
