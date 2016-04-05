34b253f87439cce89749cf1cf5e90615e093c5be
Maira Bello <maira.araujo@liferay.com>
2016-04-05 16:34:38 -0300

    Completely hides the tooltip via display none after animation ends

--- src/Tooltip.js
+++ src/Tooltip.js
@@ -1,5 +1,6 @@
 'use strict';
 
+import dom from 'metal-dom';
 import Soy from 'metal-soy';
 import TooltipBase from './TooltipBase';
 import templates from './Tooltip.soy';
@@ -9,11 +10,29 @@ import templates from './Tooltip.soy';
  */
 class Tooltip extends TooltipBase {
 	/**
+	 * Hides the alert completely (with display "none"). This is called after the
+	 * hiding animation is done.
+	 * @protected
+	 */
+	hideCompletely_() {
+		if (!this.visible) {
+			this.element.style.display = 'none';
+		}
+	}
+
+	/**
 	 * State synchronization logic for `visible`. Updates the element's opacity,
 	 * since bootstrap uses opacity instead of display for tooltip visibility.
 	 * @param {boolean} visible
 	 */
 	syncVisible(visible) {
+		if (!visible) {
+			dom.once(this.element, 'animationend', this.hideCompletely_.bind(this));
+			dom.once(this.element, 'transitionend', this.hideCompletely_.bind(this));
+		} else {
+			this.element.style.display = '';
+		}
+
 		this.element.style.opacity = visible ? 1 : '';
 		super.syncVisible(visible);
 	}
